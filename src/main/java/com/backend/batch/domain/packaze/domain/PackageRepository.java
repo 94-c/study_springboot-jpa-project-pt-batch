0b2c5152e285b0a88d4af7c2c3fb8d137f5ad222
94-c <hyeongwoo26@gmail.com>
2024-02-14 13:09:11 +0900

    #3 - jpa 테스트

---
+++ src/main/java/com/backend/batch/domain/packaze/domain/PackageRepository.java
@@ -0,0 +1,7 @@
+package com.backend.batch.domain.packaze.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PackageRepository extends JpaRepository<PackageEntity, Integer> {
+
+}
---
+++ src/test/java/com/backend/batch/domain/packaze/domain/PackageRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.backend.batch.domain.packaze.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PackageRepositoryTest {
+
+}
