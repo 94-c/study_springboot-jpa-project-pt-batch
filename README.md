3717306db910614b235bc1ee2182cdd34a5d6807
94-c <hyeongwoo26@gmail.com>
2024-02-14 10:51:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# PT 이용권 관리 서비스
+
+- spring boot jpa & batch를 활용한 PT 이용권 관리 서비스
+  - 이용권 만료, 일괄 지급, 수업 전 알림, 수업 후 이용권 차감 기능 제공
+
+### Environments
+
+----------------
+- Open JDK 17
+- Spring Boot 3.1.8
+- Gradle
+- MySQL 
+- JPA
+- Lombok
+- ModelMapper
+
+### Database
+
+--------------
+
---
+++ src/main/java/com/backend/batch/ProjectPtBatchApplication.java
@@ -0,0 +1,13 @@
+package com.backend.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPtBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPtBatchApplication.class, args);
+    }
+
+}
