5ffae74016622765568385a3c9fba2c54ad0db26
94-c <hyeongwoo26@gmail.com>
2024-02-14 13:08:10 +0900

    #5 - batch config 생성, application 설정

--- src/main/java/com/backend/batch/ProjectPtBatchApplication.java
+++ src/main/java/com/backend/batch/ProjectPtBatchApplication.java
@@ -1,10 +1,41 @@
 package com.backend.batch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjectPtBatchApplication {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public ProjectPtBatchApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Step passStep() {
+        return this.stepBuilderFactory.get("passStep")
+                .tasklet(((contribution, chunkContext) -> {
+                    System.out.println("Execute PassStep");
+                    return RepeatStatus.FINISHED;
+                })).build();
+    }
+
+    @Bean
+    public Job passJob() {
+        return this.jobBuilderFactory.get("passJob")
+                .start(passStep())
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectPtBatchApplication.class, args);
